ec8426fc4ca0dddfad5cfbcf084f0bc80f139c67
Jhon Ochoa <jhon.ochoa@openintl.com>
2024-06-18 20:37:18 -0500

    initial commit

---
+++ src/main/java/com/technical/support/datasupport/DatasupportApplication.java
@@ -0,0 +1,13 @@
+package com.technical.support.datasupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technical/support/datasupport/DatasupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technical.support.datasupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
