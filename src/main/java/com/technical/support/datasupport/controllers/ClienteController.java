c4f0a1fc0a74ca2fc655ed70ae22ba0274d9ee7c
Jhon Ochoa <jhon.ochoa@openintl.com>
2024-07-03 20:54:15 -0500

    se obtiene cliente por id y todos

--- src/main/java/com/technical/support/datasupport/controllers/ClienteController.java
+++ src/main/java/com/technical/support/datasupport/controllers/ClienteController.java
@@ -5,10 +5,10 @@ import com.technical.support.datasupport.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cliente")
@@ -22,5 +22,16 @@ public class ClienteController {
         clienteRepository.save(cliente);
         return new ResponseEntity(HttpStatus.OK);
 
+    }
+
+    @GetMapping("/all")
+    public List<Cliente> getClientes(){
+        return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cliente getCliente(@PathVariable Long id){
+        Optional<Cliente> clienteOptional = clienteRepository.findById(id);
+        return clienteOptional.isPresent() ?  clienteOptional.get() : null;
     }
 }
--- src/main/java/com/technical/support/datasupport/repositories/ClienteRepository.java
+++ src/main/java/com/technical/support/datasupport/repositories/ClienteRepository.java
@@ -3,5 +3,9 @@ package com.technical.support.datasupport.repositories;
 import com.technical.support.datasupport.entities.Cliente;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+    @Override
+    List<Cliente> findAll();
 }
