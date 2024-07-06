4d61f9725f0a2d8407c3e8abcece7602795c769c
Jhon Ochoa <jhon.ochoa@openintl.com>
2024-07-06 09:51:04 -0500

    add cros 18

--- src/main/java/com/technical/support/datasupport/config/SecurityConfiguration.java
+++ src/main/java/com/technical/support/datasupport/config/SecurityConfiguration.java
@@ -51,7 +51,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://20.55.68.236:5173"));
+        configuration.setAllowedOrigins(List.of("http://20.55.68.236:5173", "http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET","POST"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
         configuration.addAllowedOrigin("*");
